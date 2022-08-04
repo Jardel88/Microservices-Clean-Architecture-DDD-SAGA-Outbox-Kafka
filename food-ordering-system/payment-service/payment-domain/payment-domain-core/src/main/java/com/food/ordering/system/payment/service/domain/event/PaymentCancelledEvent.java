a1b7074ef49e3fa2c12650f88e9a98267f4d7cc8
jardel88 <jardelsilvagomes@hotmail.com>
2022-08-04 13:53:43 -0300

    payment-domain-core Add Exceptions e eventos de dominio

---
+++ food-ordering-system/payment-service/payment-domain/payment-domain-core/src/main/java/com/food/ordering/system/payment/service/domain/event/PaymentCancelledEvent.java
@@ -0,0 +1,15 @@
+package com.food.ordering.system.payment.service.domain.event;
+
+import com.food.ordering.system.domain.event.publisher.DomainEventPublisher;
+import com.food.ordering.system.payment.service.domain.entity.Payment;
+
+import java.time.ZonedDateTime;
+import java.util.Collections;
+import java.util.List;
+
+public class PaymentCancelledEvent extends PaymentEvent {
+
+    public PaymentCancelledEvent(Payment payment, ZonedDateTime createdAt, List<String> failureMessages) {
+        super(payment, createdAt, failureMessages);
+    }
+}
